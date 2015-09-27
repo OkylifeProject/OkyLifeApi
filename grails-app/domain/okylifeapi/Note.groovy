8c9acc6243ef2ff9da4ba4465ae2950957c90402
Alejandro Velasco <savelascod@unal.edu.co>
2015-09-27 11:28:57 -0500

    -Creados atributos y constraints para la clase Note. -Editado archivo gitignore

--- grails-app/domain/okylifeapi/Note.groovy
+++ grails-app/domain/okylifeapi/Note.groovy
@@ -1,7 +1,16 @@
 package okylifeapi
 
 class Note {
+    String content
+    Date publicationDate
+    String imagePath
 
     static constraints = {
+        imagePath nullable: true, blank: false
+        content nullable: false, blank: false
+        publicationDate validator: { val ->
+            Date currentDate = new Date()
+            if (val < currentDate) return ['datePriorTo', val.toString(), currentDate.toString()]
+        }
     }
 }

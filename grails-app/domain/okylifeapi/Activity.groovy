240621f22efbb3d883127105e804fd0fef7e60e5
Alejandro Velasco <savelascod@unal.edu.co>
2015-09-27 11:14:33 -0500

    -Creados atributos y constraints para la clase Activity.

--- grails-app/domain/okylifeapi/Activity.groovy
+++ grails-app/domain/okylifeapi/Activity.groovy
@@ -2,6 +2,14 @@ package okylifeapi
 
 class Activity {
 
+    String activityType;
+    Date creationDate;
+
     static constraints = {
+        creationDate nullable: false, validator: { val ->
+            Date currentDate = new Date()
+            if (val < currentDate) return ['datePriorTo', val.toString(), currentDate.toString()]
+        }
+        activityType nullable: false
     }
 }

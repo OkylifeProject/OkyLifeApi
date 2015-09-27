6af5a55200b3ad450bc80163ef521865fa893a03
Alejandro Velasco <savelascod@unal.edu.co>
2015-09-27 11:34:35 -0500

    -Creados atributos y constraints para la clase Okibar

--- grails-app/domain/okylifeapi/OkiBar.groovy
+++ grails-app/domain/okylifeapi/OkiBar.groovy
@@ -1,7 +1,25 @@
 package okylifeapi
 
 class OkiBar {
+    double calories
+    double steps
+    double speed
+    double hydration
+    double duration
+    double rythm
+    double cardiacFrequency
+    double cadence
+    double objetiveDistance
 
     static constraints = {
+        calories min: (double) 0
+        steps min: (double) 0
+        speed min: (double) 0
+        hydration min: (double) 0
+        duration min: (double) 0
+        rythm min: (double) 0
+        cardiacFrequency min: (double) 0
+        cadence min: (double) 0
+        objetiveDistance min: (double) 0
     }
 }

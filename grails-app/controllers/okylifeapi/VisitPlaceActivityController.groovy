63347cf764a95950f7fbdfc2d34c278111a7a34e
Alejandro Velasco <savelascod@unal.edu.co>
2015-10-31 21:49:55 -0500

    -Agregados medodos setVisitPlaceActivityFields, getVisitPlaceActivityByUser, createVisitPlaceActivity

--- grails-app/controllers/okylifeapi/VisitPlaceActivityController.groovy
+++ grails-app/controllers/okylifeapi/VisitPlaceActivityController.groovy
@@ -1,6 +1,12 @@
 package okylifeapi
 
+import grails.converters.JSON
 import grails.transaction.Transactional
+import org.apache.commons.validator.routines.EmailValidator
+import org.codehaus.groovy.grails.web.json.JSONArray
+import org.codehaus.groovy.grails.web.json.JSONObject
+
+import java.text.SimpleDateFormat
 
 import static org.springframework.http.HttpStatus.*
 
@@ -20,6 +26,93 @@ class VisitPlaceActivityController {
 
     def create() {
         respond new VisitPlaceActivity(params)
+    }
+
+    def getVisitPlaceActivitiesByUser(String email) {
+        EmailValidator emailValidator = EmailValidator.getInstance()
+        if (emailValidator.isValid(email)) {
+            def userInstance = User.findByEmail(email)
+            if (userInstance) {
+                def userActivities = VisitPlaceActivity.findAllByUser(userInstance)
+                if (userActivities) {
+                    JSONArray jsonArray = new JSONArray()
+                    userActivities.each {
+                        JSONObject jsonObject = new JSONObject()
+                        jsonObject.put("id", it.getId())
+                        jsonObject.put("name", it.getName())
+                        jsonObject.put("description", it.getDescription())
+                        jsonObject.put("creationDate", it.creationDate)
+                        jsonObject.put("type", it.type)
+                        jsonObject.put("address", it.address)
+                        jsonObject.put("distance", it.distance)
+                        jsonArray.put(jsonObject)
+                    }
+                    render jsonArray as JSON
+                } else {
+                    response.status = 404
+                    render "User doesn't have Visit Place Activities"
+                }
+            } else {
+                response.status = 404
+                render "User doesnt exists"
+            }
+        } else {
+            response.status = 404
+            render "Invalid email"
+        }
+    }
+
+    def createVisitPlaceActivity(String email) {
+        EmailValidator emailValidator = EmailValidator.getInstance()
+        if (!emailValidator.isValid(email)) {
+            response.status = 404
+            render "Invalid Email"
+            return
+        }
+        def userInstance = User.findByEmail(email)
+        if (!userInstance) {
+            response.status = 404
+            render "User doesnt exists"
+            return
+        }
+        SimpleDateFormat format = new SimpleDateFormat("MM/dd/yy")
+        //TODO: Delete OkyBar from constructor
+        def visitPlace = new VisitPlaceActivity(creationDate: format.parse(format.format(new Date())), description: params.description, name: params.name, user: userInstance, type: params.type, okiBar: new OkiBar())
+        visitPlace.save(flush: true)
+        if (!visitPlace.hasErrors()) {
+            if (params.longitude && params.latitude) {
+                def location = new Location(longitude: Double.valueOf(params.longitude), latitude: Double.valueOf(params.latitude), activity: visitPlace)
+                location.save(flush: true)
+            }
+            render "Success"
+        } else if (visitPlace.hasErrors()) {
+            render "There are several data errors; please verify and re-send the information\n" + visitPlace.errors.getAllErrors()
+        }
+    }
+
+    def setVisitPlaceActivityFields(long visitPlaceActivityId) {
+        def visitPlace = VisitPlaceActivity.findById(visitPlaceActivityId)
+        if (visitPlace) {
+            if (params.type) {
+                visitPlace.type = Double.valueOf(params.type)
+            }
+            if (params.address) {
+                visitPlace.address = Double.valueOf(params.address)
+            }
+            if (params.distance) {
+                visitPlace.distance = Double.valueOf(params.distance)
+            }
+            visitPlace.save(flush: true)
+            if (visitPlace.hasErrors()) {
+                response.status = 505
+                render "Error at saving parameters; please check inputs"
+            } else {
+                render "Success"
+            }
+        } else {
+            response.status = 404
+            render "VisitPlace Activity Not Found"
+        }
     }
 
     @Transactional

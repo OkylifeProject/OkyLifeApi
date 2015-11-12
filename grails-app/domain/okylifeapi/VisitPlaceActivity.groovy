4bcfd037682b04497e152aaa52afb8b690eda9ac
Alejandro Velasco <savelascod@unal.edu.co>
2015-11-12 13:21:30 -0500

    -removed writing mistake

--- grails-app/domain/okylifeapi/VisitPlaceActivity.groovy
+++ grails-app/domain/okylifeapi/VisitPlaceActivity.groovy
@@ -11,6 +11,6 @@ class VisitPlaceActivity extends Activity {
         calories min: (double) 0
         type nullable: true
         address nullable: true
-        type inList: ["Park", "Hospital", "Museum", "Store", "Restaurant"] - re
+        type inList: ["Park", "Hospital", "Museum", "Store", "Restaurant"]
     }
 }

fe5cb7f2c1fbfc8e5601a78ce50a8977eac38016
Alejandro Velasco <savelascod@unal.edu.co>
2015-11-12 13:20:32 -0500

    -Added calories to VisitPlaceActivity

--- grails-app/domain/okylifeapi/VisitPlaceActivity.groovy
+++ grails-app/domain/okylifeapi/VisitPlaceActivity.groovy
@@ -4,9 +4,11 @@ class VisitPlaceActivity extends Activity {
     double distance
     String address
     String type
+    double calories
 
     static constraints = {
         distance min: (double) 0
+        calories min: (double) 0
         type nullable: true
         address nullable: true
         type inList: ["Park", "Hospital", "Museum", "Store", "Restaurant"] - re
